5f99f1684d0622063d31f1313d037c004bdbacf4
BaherAbdelfatah <baherbeho_ls@yahoo.com>
2020-04-06 02:22:11 +0200

    Implement Conditional Navigation !

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -22,7 +22,7 @@ class TitleFragment : Fragment() {
         val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater, R.layout.fragment_title, container, false)
         binding.playButton.setOnClickListener(
                 Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
-        ) 
+        )
         return binding.root
     }
 
